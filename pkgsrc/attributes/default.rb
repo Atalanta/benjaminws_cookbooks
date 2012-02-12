a1c6645a7b21a736e395c7128eeccd89b059855d
Atalanta Systems Engineering <support@atalanta-systems.com>
2012-02-12 20:40:39 +0000

    Stub for linux client recipe. Add attributes to support Linux client recipe. Move documentation to markdown format, and expand.

---
+++ pkgsrc/attributes/default.rb
@@ -0,0 +1,3 @@
+default['pkgsrc']['package_server'] = 'http://pkgsrc.atalanta-systems.com'
+default['pkgsrc']['bootstrap_tarball']['url'] = 'http://pkgsrc.atalanta-systems.com/pkgsrc-bootstrap.tar'
+default['pkgsrc']['bootstrap_tarball']['checksum'] = 'f3216fb24eaab37bccdadad44be518b9'
--- pkgsrc/metadata.rb
+++ pkgsrc/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Benjamin W. Smith"
-maintainer_email "benjaminwarfield@just-another.net"
-license          "All rights reserved"
-description      "Use pkgin to maintain solaris binary pkgsrc packages."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+maintainer       "Atalanta Systems Engineering"
+maintainer_email "support@atalanta-systems.com"
+license          "Apache 2.0"
+description      "Provides infrastructure to manage binary packages with pkgsrc/pkgin"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.2"
